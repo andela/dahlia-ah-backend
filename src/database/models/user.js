b367f2721cd55da88ae7149205ed199d774dc3e7
Osaukhumwen Iyamuosa <osaukhu.bi@gmail.com>
2019-07-24 16:51:59 +0100

    chore(postgres):configure postgres
    [Finishes #167164969]

---
+++ src/database/models/user.js
@@ -0,0 +1,9 @@
+export default (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    username: DataTypes.STRING
+  }, {});
+  User.associate = () => {
+    // associations can be defined here
+  };
+  return User;
+};
